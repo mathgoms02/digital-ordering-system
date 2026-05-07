c46b176b52768539442dac55c060da889b34769d
mathgoms02 <math.gomsx3@gmail.com>
2026-05-07 11:11:33 -0300

    feat: Created validation for ticket-item post

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -11,3 +11,10 @@ class TicketItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TicketItem
         fields = "__all__"
+
+
+    # Função precida ter o nome da FK para chamar dado da outra tabela
+    def validate_ticket(self, value):
+        if value.status == "C":
+            raise serializers.ValidationError("Comanda já fechada!")
+        return value
