cb1dcc2d142a182d7d6f21f5f13976a594ea28bd
mathgoms02 <math.gomsx3@gmail.com>
2026-05-06 22:06:59 -0300

    feat(back): Created models for ticket and ticketitem

--- products/models.py
+++ products/models.py
@@ -12,25 +12,3 @@ class Product(models.Model):
     category = models.CharField(max_length=30, choices=PRODUCTS_CATEGORY)
     # Decimal is better to work with money
     unit_value = models.DecimalField(max_digits=19, decimal_places=2)
-
-
-class Ticket(models.Model):
-    STATUS_CHOICES = [
-        ("O", "Open"),
-        ("C", "Closed"),
-    ]
-
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
-    table = models.CharField(max_length=10)
-    client_name = models.CharField(max_length=255)
-    client_phone = models.CharField(max_length=20) # TODO: Adicionar validator regexp
-    observation = models.CharField(max_length=255)
-    # bartender = models.ForeignKey(Users, on_delete=models.CASCADE) TODO: Create Model User
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-class TicketItem(models.Model):
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    unit_value = models.DecimalField(max_digits=19, decimal_places=2)
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductsViewSet
+from .views import ProductViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductsViewSet)
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
